f80246cc3dce41557c68f1dba167ab61381d6f78
Algerina <kh.amelle@gmail.com>
2023-10-19 19:55:16 +0100

    Add search movies component

---
+++ app/components/MovieSearch.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+
+const MovieSearch = () => {
+  const [movie, setMovies] = useState([]);
+
+  return <div>MovieSearch</div>;
+};
+
+export default MovieSearch;
