91d5b057cc9a474bb2a2ed63cffbafdb20983a27
Algerina <kh.amelle@gmail.com>
2023-10-21 20:00:50 +0100

    Create serchMovie function

--- app/services/movie.service.ts
+++ app/services/movie.service.ts
@@ -7,3 +7,21 @@ const client = axios.create({
   baseURL: API_BASE_URL,
   params: { api_key: API_key },
 });
+
+export const searchMovies = async{query: string} => {
+  try{
+    const response = await client.get("/search/movie", {
+      params: {query},
+    });
+
+    if (response.data && response.data.results) {
+      return response.data.results;
+    } else {
+      console.error("Couldn't get the data");
+    }
+
+  } catch(error){
+console.error(error);
+return[];
+  }
+}
