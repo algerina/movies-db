d36ff8bc8bc2dadd6129b206a2d03d078d7a37a7
Algerina <kh.amelle@gmail.com>
2023-10-23 17:28:17 +0100

    Finnish the implementation of the Search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -4,12 +4,27 @@ interface SearchBarProps {
   onSearch: (query: string) => void;
 }
 
-const SearchBar = () => {
+const SearchBar = (onSearch: SearchBarProps) => {
+  const [query, setQuery] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSearch(query);
+  };
   return (
-    <form className="flex items-center w-screen max-w-3xl">
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center w-screen max-w-3xl"
+    >
       <input
         type="text"
-        className="border border-gray-300 rounded-1 py-2 px-4 focus:outline-none focus:border-500-blue "
+        className="border border-gray-300 rounded-1 py-2 px-4 focus:outline-none focus:border-blue-500 "
+        value={query}
+        onChange={handleChange}
       />
     </form>
   );
